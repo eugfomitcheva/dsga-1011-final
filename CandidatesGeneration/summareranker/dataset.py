233bb97495a03be0f9f8573e4aa361ec0c8ca707
Varsha Vattikonda <vv2116@nyu.edu>
2022-11-15 16:08:02 -0500

    Corrected version of candidate generation

--- CandidatesGeneration/summareranker/dataset.py
+++ CandidatesGeneration/summareranker/dataset.py
@@ -5,13 +5,14 @@ from time import time
 
 
 class MultitaskRerankingDataset:
-    def __init__(self, mode, tokenizer, texts, scored_summaries, labels, args):
+    def __init__(self, mode, tokenizer, texts, scored_summaries, labels, max_len,max_summary_len):
         self.mode = mode
         self.tokenizer = tokenizer
         self.texts = texts
         self.scored_summaries = scored_summaries
         self.labels = labels
-        self.args = args
+        self.max_length = max_len
+        self.max_summary_length = max_summary_len
 
     def __len__(self):
         return len(self.texts)
@@ -32,18 +33,18 @@ class MultitaskRerankingDataset:
                 for j in range(len(summary_scores[i])):
                     summary_scores[i][j] *= 30
 
-        text_inputs = self.tokenizer(text, return_tensors="pt", max_length=self.args.max_length, padding='max_length')
-        text_inputs["input_ids"] = text_inputs["input_ids"][:, :self.args.max_length]
-        text_inputs["attention_mask"] = text_inputs["attention_mask"][:, :self.args.max_length]
-        summary_candidates_inputs = self.tokenizer(summary_candidates, return_tensors="pt", truncation=True, max_length=self.args.max_summary_length, padding='max_length')
-        summary_candidates_inputs["input_ids"] = summary_candidates_inputs["input_ids"][:,:self.args.max_summary_length]
-        summary_candidates_inputs["attention_mask"] = summary_candidates_inputs["attention_mask"][:,:self.args.max_summary_length]
+        text_inputs = self.tokenizer(text, return_tensors="pt", max_length=self.max_length, padding='max_length')
+        text_inputs["input_ids"] = text_inputs["input_ids"][:, :self.max_length]
+        text_inputs["attention_mask"] = text_inputs["attention_mask"][:, :self.max_length]
+        summary_candidates_inputs = self.tokenizer(summary_candidates, return_tensors="pt", truncation=True, max_length=self.max_summary_length, padding='max_length')
+        summary_candidates_inputs["input_ids"] = summary_candidates_inputs["input_ids"][:,:self.max_summary_length]
+        summary_candidates_inputs["attention_mask"] = summary_candidates_inputs["attention_mask"][:,:self.max_summary_length]
 
-        text_and_summaries = [self.tokenizer.decode(text_inputs["input_ids"][0], skip_special_tokens=True) + " " + self.args.sep_symbol + " " \
+        text_and_summaries = [self.tokenizer.decode(text_inputs["input_ids"][0], skip_special_tokens=True) + " " + self.sep_symbol + " " \
                               + self.tokenizer.decode(summary_candidates_inputs["input_ids"][i], skip_special_tokens=True) for i in range(len(summary_candidates_inputs["input_ids"]))]
-        text_and_summaries_inputs = self.tokenizer(text_and_summaries, return_tensors="pt", truncation=True, max_length=self.args.max_length + self.args.max_summary_length, padding='max_length')
-        text_and_summaries_inputs["input_ids"] = text_and_summaries_inputs["input_ids"][:, :(self.args.max_length + self.args.max_summary_length)]
-        text_and_summaries_inputs["attention_mask"] = text_and_summaries_inputs["attention_mask"][:, :(self.args.max_length + self.args.max_summary_length)]
+        text_and_summaries_inputs = self.tokenizer(text_and_summaries, return_tensors="pt", truncation=True, max_length=self.max_length + self.max_summary_length, padding='max_length')
+        text_and_summaries_inputs["input_ids"] = text_and_summaries_inputs["input_ids"][:, :(self.max_length + self.max_summary_length)]
+        text_and_summaries_inputs["attention_mask"] = text_and_summaries_inputs["attention_mask"][:, :(self.max_length + self.max_summary_length)]
 
         scores = torch.cat([torch.tensor(summary_scores[i]).unsqueeze(0) for i in range(len(summary_scores))], 0)
         labels = torch.max(scores, dim = 1)[0]
@@ -66,22 +67,23 @@ class MultitaskRerankingDataset:
 
 
 class MultitaskRerankingDatasetTrain:
-    def __init__(self, mode, tokenizer, texts, scored_summaries, labels, args):
+    def __init__(self, mode, tokenizer, texts, summaries, labels,scores, max_len,max_summary_len):
         self.mode = mode
         self.tokenizer = tokenizer
         self.texts = texts
-        self.scored_summaries = scored_summaries
+        self.summaries = summaries
         self.labels = labels
-        self.args = args
+        self.scores = scores
+        self.max_length = max_len
+        self.max_summary_length = max_summary_len
 
     def __len__(self):
         return len(self.texts)
 
     def __getitem__(self, item):
         text = self.texts[item]
-        scored_summaries = self.scored_summaries[item]
-        summary_candidates = scored_summaries[0]
-        summary_scores = scored_summaries[1]
+        summary_candidates = self.summaries[item]
+        summary_scores = self.scores[item]
         for i in range(len(summary_scores)):
             # re-adjust BERTScore
             if min(summary_scores[i]) > 0.0 and max(summary_scores[i]) < 1.0:
@@ -92,19 +94,19 @@ class MultitaskRerankingDatasetTrain:
                 for j in range(len(summary_scores[i])):
                     summary_scores[i][j] *= 30
 
-        text_inputs = self.tokenizer(text, return_tensors="pt", max_length=self.args.max_length, padding='max_length')
-        text_inputs["input_ids"] = text_inputs["input_ids"][:, :self.args.max_length]
-        text_inputs["attention_mask"] = text_inputs["attention_mask"][:, :self.args.max_length]
+        text_inputs = self.tokenizer(text, return_tensors="pt", max_length=self.max_length, padding='max_length')
+        text_inputs["input_ids"] = text_inputs["input_ids"][:, :self.max_length]
+        text_inputs["attention_mask"] = text_inputs["attention_mask"][:, :self.max_length]
 
-        summary_candidates_inputs = self.tokenizer(summary_candidates, return_tensors="pt", truncation=True, max_length=self.args.max_summary_length, padding='max_length')
-        summary_candidates_inputs["input_ids"] = summary_candidates_inputs["input_ids"][:,:self.args.max_summary_length]
-        summary_candidates_inputs["attention_mask"] = summary_candidates_inputs["attention_mask"][:,:self.args.max_summary_length]
+        summary_candidates_inputs = self.tokenizer(summary_candidates, return_tensors="pt", truncation=True, max_length=self.max_summary_length, padding='max_length')
+        summary_candidates_inputs["input_ids"] = summary_candidates_inputs["input_ids"][:,:self.max_summary_length]
+        summary_candidates_inputs["attention_mask"] = summary_candidates_inputs["attention_mask"][:,:self.max_summary_length]
 
-        text_and_summaries = [self.tokenizer.decode(text_inputs["input_ids"][0], skip_special_tokens=True) + " " + self.args.sep_symbol + " " \
+        text_and_summaries = [self.tokenizer.decode(text_inputs["input_ids"][0], skip_special_tokens=True) + " " + self.sep_symbol + " " \
                               + self.tokenizer.decode(summary_candidates_inputs["input_ids"][i], skip_special_tokens=True) for i in range(len(summary_candidates_inputs["input_ids"]))]
-        text_and_summaries_inputs = self.tokenizer(text_and_summaries, return_tensors="pt", truncation=True, max_length=self.args.max_length + self.args.max_summary_length, padding='max_length')
-        text_and_summaries_inputs["input_ids"] = text_and_summaries_inputs["input_ids"][:, :(self.args.max_length + self.args.max_summary_length)]
-        text_and_summaries_inputs["attention_mask"] = text_and_summaries_inputs["attention_mask"][:, :(self.args.max_length + self.args.max_summary_length)]
+        text_and_summaries_inputs = self.tokenizer(text_and_summaries, return_tensors="pt", truncation=True, max_length=self.max_length + self.max_summary_length, padding='max_length')
+        text_and_summaries_inputs["input_ids"] = text_and_summaries_inputs["input_ids"][:, :(self.max_length + self.max_summary_length)]
+        text_and_summaries_inputs["attention_mask"] = text_and_summaries_inputs["attention_mask"][:, :(self.max_length + self.max_summary_length)]
 
         scores = torch.cat([torch.tensor(summary_scores[i]).unsqueeze(0) for i in range(len(summary_scores))], 0)
         labels = torch.max(scores, dim = 1)[0]
